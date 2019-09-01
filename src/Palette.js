22abe7c128f71dd5f69c6d584395a02ef0b40a44
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-09-01 21:45:47 +0800

    added Palette.js

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* NAV goes here */}
+                <div className="Palette-colours">
+                    {/* bunch of colour boxes */}
+                </div>
+                {/* footer eventually */}
+            </div>
+        );
+    }
+}
+
+export default Palette;
