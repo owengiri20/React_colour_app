de4c8365ea7c3f8239b66a2a1274a9fc074c68d4
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-09-12 09:43:33 +0800

    installed material ui, used select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Slider, { Range } from 'rc-slider';
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem';
 import 'rc-slider/assets/index.css';
 import "./Navbar.css"
 
@@ -17,6 +19,13 @@ class Navbar extends Component {
                         <Slider defaultValue={level} min={100} max={900}
                             step={100} onAfterChange={changeLevel} />
                     </div>
+                </div>
+                <div className="select-container">
+                    <Select>
+                        <MenuItem value="hex">#f4f4f4</MenuItem>
+                        <MenuItem value="rgb">rgb(255,55,67)</MenuItem>
+                        <MenuItem value="rgba">rgb(255,55,67,0.2)</MenuItem>
+                    </Select>
                 </div>
 
 
